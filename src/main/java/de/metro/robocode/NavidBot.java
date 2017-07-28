120593651126d5f6ce1930233925433d13878a98
Navid Abbaszadeh <navid.abbaszadeh@gmail.com>
2017-07-28 16:32:16 +0200

    hi

--- src/main/java/de/metro/robocode/NavidBot.java
+++ src/main/java/de/metro/robocode/NavidBot.java
@@ -1,29 +1,73 @@
 package de.metro.robocode;
 
 import robocode.*;
+import robocode.Robot;
+
+import java.awt.*;
 
 public class NavidBot extends Robot {
 
+
+    boolean init = false;
+    double W,H;
+    boolean targetFound = false;
+
+
+    void doInit() {
+        if(init) return;
+        init = true;
+        setColors(Color.red, Color.BLACK.white, Color.green);
+        W = getBattleFieldWidth();
+        H = getBattleFieldHeight();
+    }
+
     @Override
     public void run() {
+
+        doInit();
 
         double radius = 100.0;
         double angle = 90.0;
 
         while (true) {
-            ahead(radius);
-            turnLeft(angle);
-            turnGunLeft(angle);
-            fireBullet(getEnergy());
+            back(40);
+            ahead(40);
+
+            if(targetFound) {targetFound = false;} else { turnRadarLeft(45);}
+            scan();
         }
     }
 
     public void onScannedRobot(ScannedRobotEvent e) {
-        fire(1);
+        targetFound = true;
+        double targetBearing = e.getBearing();
+        double absoluteBearing = this.getHeading() + targetBearing;
+        //out.print("absolute bearing " + absoluteBearing);
+        //out.println("target bearning " + targetBearing);
+        double targetDist = e.getDistance();
+        double targetHeading = e.getHeading();
+        double targetVelocity = e.getVelocity();
+
+        //out.println("gun heading: " + getGunHeading());
+        myTurnGun(absoluteBearing - getGunHeading() );
+        fire(3);
     }
+
+
 
     public void onHitByBullet(HitByBulletEvent e) {
-        turnLeft(90 - e.getBearing());
+
     }
 
+
+    void myTurnGun( double deg) {
+        int intDeg = (int) Math.ceil(deg + 360);
+        intDeg = intDeg % 360;
+        //out.println( "turning degrees "+ intDeg );
+        if(intDeg <180) {
+            turnGunRight( intDeg );
+        }else {
+            turnGunLeft( 360 - intDeg );
+        }
+    }
 }
